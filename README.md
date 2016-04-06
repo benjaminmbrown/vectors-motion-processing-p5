4ef38b1f3686c89da18356fe008562831e80fd5a
Benjamin M. Brown <benjaminbb@gmail.com>
2016-04-06 15:13:30 -0500

    Adding mover object that simply moves w a static vector

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vectors-motion-processing-p5
-Exploring motion and vectors in Processing (p5.js)
+Exploring motion and vectors in Processing (p5.js). Created a mover class to be more OO.
+
+<img src="http://zippy.gfycat.com/GrizzledAdeptBufflehead.gif"/>
--- mover.js
+++ mover.js
@@ -1,18 +1,34 @@
-class Mover {
+var Mover = function() {
 	
 		this.position = createVector(random(width),random(height));
 		this.velocity = createVector(random(-2,2),random(-2,2));
 
 		this.display = function(){
-
+			stroke(0);
+			strokeWeight(2);
+			fill(127);
+			ellipse(this.position.x, this.position.y, 48,48);
 		}
 
 		this.checkEdges = function(){
+			if(this.position.x > width){
+				this.position.x = 0;
+			}
+			else if(this.position.x <0){
+				this.position.x = width;
+			}
+
+			if(this.position.y > height){
+				this.position.y=0;
+			}
+			else if(this.position.y <0){
+				this.position.y  = height;
+			}
 
 		}
 
 		this.update = function(){
-			
+			this.position.add(this.velocity);
 		}
 
 
